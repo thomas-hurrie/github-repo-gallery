3dc550c4f72cedcb1055d267799277fe0d0ab28c
Thomas Hurrie <thomas.hurrie@gmail.com>
2021-06-17 09:58:01 -0400

    Fetch and display repos

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const overview = document.querySelector(".overview");
 //github username
 const username = "thomas-hurrie";
+//select repos list
+const repoList = document.querySelector(".repo-list");
 
 //fetch info from github profile
 const getGit = async function () {
@@ -27,4 +29,22 @@ const displayUserInfo = function (data) {
       </div>
     `;
     overview.append(div);
+    gitRepos();
   };
+
+  const gitRepos = async function () {
+      const fetchRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+      const repoData = await fetchRepos.json();
+      //console.log(repos);
+      displayRepos(repoData);
+  };
+
+  const displayRepos = function (repos) {
+      for (const repo of repos) {
+          const repoItem = document.createElement("li");
+          repoItem.classList.add("repo");
+          repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+          repoList.append(repoItem);
+      }
+  };
+
